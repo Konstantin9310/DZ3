af3b703907639d340eb46e0a848ed5624762c708
Konstantin <alkapone10031993@gmail.com>
2023-06-19 19:18:42 +0300

    1

---
+++ FirstWork/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.Write("Input number => ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Nuber {number} - yes.");
+  }
+  else Console.WriteLine($"Number: {number} - no .");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Input correct number");
